1aa3c9fe48d6d4fa03b4682a2d54560dd7841099
zoraaga1 <fati.sousse12@gmail.com>
2023-11-26 00:16:03 +0100

    task 11

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -11,27 +11,27 @@
  */
 int main(void)
 {
-    pid_t child_pid;
-    int i;
+	pid_t child_pid;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        child_pid = fork();
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
 
-        if (child_pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
+		if (child_pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
 
-        if (child_pid == 0)
-        {
-            printf("Zombie process created, PID: %d\n", getpid());
-            exit(0);
-        }
-    }
+		if (child_pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
 
-    infinite_while();
+	infinite_while();
 
-    return (0);
+	return (0);
 }
