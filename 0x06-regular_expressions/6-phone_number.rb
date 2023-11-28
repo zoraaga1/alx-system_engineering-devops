4faa20b06e9a5cfd0561161c3efa57635f73b44b
zoraaga1 <fati.sousse12@gmail.com>
2023-11-28 23:49:12 +0100

    task 6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -2,4 +2,4 @@
 # Ruby script that accepts one argument and pass it to a regular expression matching method.
 # The regular expression must match a 10 digit phone number
 
-puts ARGV[0].scan(/^\d{10}/).join
+puts ARGV[0].scan(/^\d{10}$/).join
