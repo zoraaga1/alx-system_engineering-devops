bd1e7838cfd4eec2d40675216479206fc30cf36d
zoraaga1 <fati.sousse12@gmail.com>
2023-11-28 23:05:50 +0100

    task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,12 +2,4 @@
 #  Ruby script that accepts one argument and pass it to a regular expression matching method.
 # only strings with more than twice t and less than 5
 
-pattern = /t{3,4}/
-input_string = ARGV[0]
-
-matches = input_string.scan(pattern)
-
-concatenated_result = matches.join
-if (3..4).include?(matches.length)
-    puts input_string
-end
+puts ARGV[0].scan(/t{2,5}n/).join
