0343ee76555a707a2ff31431c147373c647e1351
zoraaga1 <fati.sousse12@gmail.com>
2023-11-28 23:13:39 +0100

    task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,9 +2,4 @@
 #  Ruby script that accepts one argument and pass it to a regular expression matching method.
 # only strings with more than twice t and less than 5
 
-matches = ARGV[0].scan(/t{2,5}n/)
-
-# Check if the number of matches is within the desired range (2 to 4)
-if (2..4).include?(matches.length)
-  puts matches.join
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
