17e327e9d58ff55294faefda79051eecbbc05058
zoraaaga <fati.sousse12@gmail.com>
2024-01-19 23:35:17 +0100

    task0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,6 @@
 # Create the file with specified content
 
-    file { '/tmp/school/school':
-    ensure  => file,
+file { '/tmp/school':
     content => 'I love Puppet',
     mode    => '0744',
     owner   => 'www-data',
