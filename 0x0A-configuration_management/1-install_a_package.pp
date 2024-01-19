258d3cc27e1e7c2a15b9e65020e3b372a26f16ff
zoraaaga <fati.sousse12@gmail.com>
2024-01-20 00:29:34 +0100

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-# Define a class to manage the installation of Flask
-
+# Install an especific version of flask (2.1.0)
 package {'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
