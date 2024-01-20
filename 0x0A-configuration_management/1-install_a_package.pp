c9373ab458e175ca0aa89b232c036e00975f4623
zoraaaga <fati.sousse12@gmail.com>
2024-01-20 15:41:13 +0100

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install an especific version of flask (2.1.0)
-
-package {'flask':
-    ensure   => '2.1.0',
-    provider => pip3,
+# Install a specific version of Flask (2.1.0) using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
