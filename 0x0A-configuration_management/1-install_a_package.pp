70eeca99708fee548e328bcd3591a2b566730bda
zoraaaga <fati.sousse12@gmail.com>
2024-01-20 22:52:29 +0100

    task 1 fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install Flask version 2.1.0 using pip3
-package {'flask':
+# Install Flask version 2.1.0 and Werkzeug version 2.0.1 using pip3
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
