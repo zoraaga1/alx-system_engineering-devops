b3827ac4eee5013fa67e5f97741b389eee257405
zoraaga1 <fati.sousse12@gmail.com>
2024-04-11 17:40:37 +0000

    task1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+"""
+top_ten
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Print the titles of the first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"limit": 10}  # Limit the number of posts to 10
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print(None)
