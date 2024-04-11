47cfc9cde8e3b6454b17b2150910dcba36755a28
zoraaga1 <fati.sousse12@gmail.com>
2024-04-11 17:00:49 +0000

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,15 @@ to get the number of subscribers for a given subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Get the number of subscribers for a given subreddit.
     Args:
         subreddit (str): The name of the subreddit.
     Returns:
-        The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+        The number of subscribers for the subreddit.
+        Returns 0 if the subreddit is invalid.
     """
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
