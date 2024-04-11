e36fbbdd27a3b3faaa89dd5b87f11600cf7f0a91
zoraaga1 <fati.sousse12@gmail.com>
2024-04-11 16:56:30 +0000

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+This module queries the Reddit API
+to get the number of subscribers for a given subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+    Args:
+        subreddit (str): The name of the subreddit.
+    Returns:
+        The number of subscribers for the subreddit. Returns 0 if the subreddit is invalid.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
